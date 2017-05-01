fccf985d4a35fb7f98050cee8dd090a866c30398
evgeniy2194 <evgeniy2194@gmail.com>
2017-05-01 21:14:59 +0300

    added config

---
+++ client/config/config.js
@@ -0,0 +1,9 @@
+const config = {
+    game: {
+        totalQuestion: 4,   //Колл-во вопросов в игре
+        playersCount: 1,    //Колл-во игроков в игре
+        roundTime: 3000     //Время одного раунда
+    }
+};
+
+export default config;
