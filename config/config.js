234e1f67e23725bb69910dff0571de7e8231f40d
evgeniy2194 <evgeniy2194@gmail.com>
2017-05-01 18:05:31 +0300

    dev

--- config/config.js
+++ config/config.js
@@ -3,12 +3,6 @@ const config = {
         port: 3000,
         appId: 5738699,
         apiSecret: "VvNbbTwC3JdsIMJYL6H3",
-    },
-    db: {
-        host: '',
-        port: '',
-        userName: '',
-        password: ''
     }
 };
 
