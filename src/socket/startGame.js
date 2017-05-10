f995ee3c5a064f49c31349e17a452ebae82d4208
evgeniy2194 <evgeniy2194@gmail.com>
2017-05-10 22:17:38 +0300

    move store creator to utils

--- src/socket/startGame.js
+++ src/socket/startGame.js
@@ -1,9 +1,7 @@
 import shuffle from 'shuffle-array';
 import Question from '../models/question';
 import Game from '../models/game';
-import GamesStore from '../store/gamesStore';
-import QueueStore from '../store/queueStore';
-import UsersStore from '../store/usersStore';
+import { GamesStore, QueueStore, UsersStore } from '../utils/store';
 import config from '../../config/config';
 import sendMessage from './sendMessage';
 import {sendUserInfo} from '../actions/userActions';
