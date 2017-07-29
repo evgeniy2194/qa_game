c8299677abcf5ec31a18b15f3ac858c2d5282226
evgeniy2194 <evgeniy2194@gmail.com>
2017-07-29 17:21:23 +0300

    dev

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -30,3 +30,35 @@ UserQuest.belongsTo(Quest, {as: 'quest'});
 UserQuest.belongsTo(User, {as: 'user'});
 
 export {Game, User, Question, QuestionAnswer};
+
+/**
+ INSERT into Questions (question) VALUES
+ ("Сколько раз старик из сказки А. С. Пушкина вызывал Золотую рыбку?"),
+ ("Сколько килобайт в 1 мегабайте?"),
+ ("Сколько пар хромосом распределяется на одного (здорового) человека?"),
+ ("В какое море впадает река Днестр?");
+
+ INSERT into QuestionAnswers (questionId, answer, isCorrect) VALUES
+ (1, "3", false),
+ (1, "4", false),
+ (1, "5", true),
+ (1, "6", false),
+ (2, "1000", false),
+ (2, "16", false),
+ (2, "512", false),
+ (2, "1024", true),
+ (3, "23", true),
+ (3, "24", false),
+ (3, "11", false),
+ (3, "12", false),
+ (4, "Мертвое", false),
+ (4, "Красное", false),
+ (4, "Синее", false),
+ (4, "Черное", true)
+
+ INSERT INTO quests (`requirements`, `check`, `rewardCount`, `rewardType`)
+ VALUES
+ (5, "SELECT count(*) FROM GameUsers WHERE userId = :userId", 1, "gems"),
+ (1, "SELECT count(*) FROM GameAnswers WHERE userId = :userId AND isCorrect = true", 2, "gems")
+
+*/
--- src/database/models/question.js
+++ src/database/models/question.js
@@ -16,29 +16,3 @@ const Question = connect.define('Question', {
 });
 
 export default Question;
-
-/**
- INSERT into Questions (question) VALUES
- ("Сколько раз старик из сказки А. С. Пушкина вызывал Золотую рыбку?"),
- ("Сколько килобайт в 1 мегабайте?"),
- ("Сколько пар хромосом распределяется на одного (здорового) человека?"),
- ("В какое море впадает река Днестр?");
-
- INSERT into QuestionAnswers (questionId, answer, isCorrect) VALUES
- (1, "3", false),
- (1, "4", false),
- (1, "5", true),
- (1, "6", false),
- (2, "1000", false),
- (2, "16", false),
- (2, "512", false),
- (2, "1024", true),
- (3, "23", true),
- (3, "24", false),
- (3, "11", false),
- (3, "12", false),
- (4, "Мертвое", false),
- (4, "Красное", false),
- (4, "Синее", false),
- (4, "Черное", true)
-*/
