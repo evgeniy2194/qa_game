2b8989051a4dbd69f5efd1ab3d9a963e6e3ba942
evgeniy2194 <evgeniy2194@gmail.com>
2017-08-01 18:00:22 +0300

    Merge branch 'develop' of https://github.com/evgeniy2194/qa_game into develop
    # Conflicts:
    #	src/socket/onConnection.js

--- src/socket/onConnection.js
+++ src/socket/onConnection.js
@@ -2,17 +2,13 @@ import checkAuthKey from '../utils/chekAuthKey';
 import {UsersStore, GamesStore} from '../utils/store';
 import {sendUserInfo, sendQuestsInfo} from '../actions/userActions';
 import sendMessage from './sendMessage';
-import {User, Quest, UserQuest} from '../database/models';
 import restoreGame from './restoreGame';
 import {getExpToLevel} from "../utils/levelCalculation";
-import connect from '../database/connect';
-import Sequelize from 'sequelize';
-import moment from 'moment';
+import {findOrCreate, genereteRandomQuest} from '../utils/userUtils';
 
 export default function (socket) {
     const query = socket.handshake.query;
     const uid = query.uid;
-    const now = moment().format('YYYY-MM-DD HH:mm:ss');
 
     //Проверяем авторизирован ли пользователь
     if (!checkAuthKey(uid, query.authKey) && process.env.NODE_ENV === 'production') {
@@ -21,30 +17,13 @@ export default function (socket) {
     }
 
     //Ищем или создаем пользователя
-    User.findOrCreate({
-        where: {
-            uid: uid
-        },
-        defaults: {
-            uid: uid,
-            firstName: query.firstName,
-            lastName: query.lastName,
-            expToLevel: getExpToLevel(2)
-        },
-        include: [{
-            model: Quest,
-            as: 'quests',
-            through: {
-                where: {
-                    isReceivedReward: false,
-                    dateFrom: { $lte: now },
-                    dateTill: { $gt: now }
-                }
-            }
-        }]
-    }).then((data, created) => {
+    findOrCreate({
+        uid: uid,
+        firstName: query.firstName,
+        lastName: query.lastName,
+        expToLevel: getExpToLevel(2)
+    }).then(user => {
         //ToDo: Записывать в Store обьект пользователя содержащий информацию о квестах, подсказках и т.д
-        const user = data[0];
         const userId = user.id;
 
         socket.userId = userId;
@@ -60,48 +39,19 @@ export default function (socket) {
         //Если нет квестов
         if (quests.length === 0) {
             //Если нет квестов - генерируем случайный квест
-            Quest.findOne({order: [Sequelize.fn('RAND')]}).then(quest => {
-                return user.addQuest(quest, {
-                    through: {
-                        progress: 0,
-                        isDone: false,
-                        isReceivedReward: false,
-                        dateFrom: moment().format('YYYY-MM-DD HH:mm:ss'),
-                        dateTill: moment().add(12, 'hours').format('YYYY-MM-DD HH:mm:ss')
-                    }
-                });
-            }).then(() => {
-                return user.getQuests({
-                    through: {
-                        model: UserQuest,
-                        where: {
-                            isReceivedReward: false,
-                            dateFrom: {
-                                $lte: moment().format('YYYY-MM-DD HH:mm:ss')
-                            },
-                            dateTill: {
-                                $gt: moment().format('YYYY-MM-DD HH:mm:ss')
-                            }
-                        }
-                    }
-                })
+            genereteRandomQuest(user).then(() => {
+                //Получаем активные квесты
+                return getActiveQuests(user);
             }).then(quests => {
+                //Отправляем данные о квестах
                 sendMessage(socket, sendQuestsInfo(quests));
             })
         } else {
+            //Отправляем данные о квестах
             sendMessage(socket, sendQuestsInfo(quests));
         }
 
         //ToDo: Проверять активную игру в обьекте пользователя в Store
-        //Если игрок не новый и у него есть незаконченная игра
-        if (!created) {
-            const sql = "SELECT g.* FROM Games g INNER JOIN GameUsers p ON p.gameId = g.id WHERE g.finishedAt IS NULL LIMIT 1";
-            connect.query(sql, {type: Sequelize.QueryTypes.SELECT}).then((response) => {
-                const game = response[0];
-                if (game) {
-                    restoreGame(socket, GamesStore.get(game.id));
-                }
-            })
-        }
+        //restoreGame(socket, GamesStore.get(game.id));
     });
 }
--- src/utils/userUtils.js
+++ src/utils/userUtils.js
@@ -2,27 +2,96 @@
  * Created by vision on 8/1/17.
  */
 import GemsAndCoinsUsing from '../database/models/';
+import {User, Quest, UserQuest} from '../database/models';
+import moment from 'moment';
 
-export function decreaseGems(user, amount){
-
-}
-
-export function increaseGems(user, amount){
+export function decreaseGems(user, amount) {
 
 }
 
-export function decreaseCoins(user, amount){
+export function increaseGems(user, amount) {
 
 }
 
-export function increaseCoins(user, amount){
+export function decreaseCoins(user, amount) {
 
 }
 
-function _changeGemsAmount(){
+export function increaseCoins(user, amount) {
 
 }
 
-function _changeCoinsAmount(){
+/**
+ * Find exists user or create new
+ * Include Quests array
+ *
+ * @param data object
+ */
+export function findOrCreate(data) {
+    const now = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    return User.findOrCreate({
+        where: {
+            uid: data.uid
+        },
+        defaults: data,
+        include: [{
+            model: Quest,
+            as: 'quests',
+            through: {
+                where: {
+                    isReceivedReward: false,
+                    dateFrom: {$lte: now},
+                    dateTill: {$gt: now}
+                }
+            }
+        }]
+    }).then(response => {
+        return response[0];
+    });
+}
+
+/**
+ * Generete random quest for user
+ *
+ * @param user
+ * @returns {Promise}
+ */
+export function genereteRandomQuest(user) {
+    return Quest.findOne({
+        order: [Sequelize.fn('RAND')]
+    }).then(quest => {
+        return user.addQuest(quest, {
+            through: {
+                progress: 0,
+                isDone: false,
+                isReceivedReward: false,
+                dateFrom: moment().format('YYYY-MM-DD HH:mm:ss'),
+                dateTill: moment().add(12, 'hours').format('YYYY-MM-DD HH:mm:ss')
+            }
+        });
+    });
+}
+
+export function getActiveQuests(user) {
+    const now = moment().format('YYYY-MM-DD HH:mm:ss');
+
+    return user.getQuests({
+        through: {
+            model: UserQuest,
+            where: {
+                isReceivedReward: false,
+                dateFrom: {$lte: now},
+                dateTill: {$gt: now}
+            }
+        }
+    });
+}
+
+function _changeGemsAmount() {
+
+}
+
+function _changeCoinsAmount() {
 
 }
